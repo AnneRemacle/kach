9d5074a13e4a256e7159a0eb9de97b0776d14986
Anne Remacle <anne.m.remacle@gmail.com>
2016-12-09 12:22:51 +0100

    Install dependencies & add base dummy modules

---
+++ static/modules/main.js
@@ -0,0 +1,13 @@
+/* ria/kach
+ *
+ * /static/modules/main.js - Main entry file
+ *
+ * Coded by Anne
+ * started at 09/12/2016
+*/
+
+import utils from "./utils";
+
+console.log( "Hey!" );
+
+utils();
---
+++ static/modules/utils.js
@@ -0,0 +1,11 @@
+/* ria/kach
+ *
+ * /static/modules/utils.js - Small utils
+ *
+ * Coded by Anne
+ * started at 09/12/2016
+*/
+
+export default function() {
+	console.log( "Hey from utils!" );
+}
