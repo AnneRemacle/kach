dc91b99a8df45e71737619c9ff74b84d89702b6a
Anne Remacle <anne.m.remacle@gmail.com>
2016-12-16 12:01:25 +0100

    Toying with Vue syntax

--- static/modules/main.js
+++ static/modules/main.js
@@ -10,8 +10,37 @@
 import Vue from "vue";
 
 let oApp = new Vue( {
-    "el": "#app",
+    "template": `
+        <div class="box">
+            <p>{{ message }}</p>
+            <ul>
+                <li v-for="cat in cats">
+                    <strong>{{ cat.name }}</strong>
+                    <span>( {{ cat.age }} )</span>
+                </li>
+            </ul>
+            <button v-on:click="revealSecret">{{ reveal.value }}</button>
+            <p v-if="secret">I'm a cat person</p>
+        </div>`,
     "data": {
         "message": "Hey from Vue!",
+        "secret": false,
+        "cats": [
+            { "name": "Argus", "age": 4 },
+            { "name": "Isis", "age": 8 },
+        ],
+        "reveal": {
+            "show": "Reveal my secrets!",
+            "hide": "Hide my secrets",
+            "value": "Reveal my secrets!",
+        },
+    },
+    "methods": {
+        "revealSecret": function() {
+            this.secret = !this.secret;
+            this.reveal.value = this.secret ? this.reveal.hide : this.reveal.show;
+        },
     },
 } );
+
+oApp.$mount( "#app" );
