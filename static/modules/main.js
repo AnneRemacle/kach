f71ba7e3e5bb20aee07a3e51df864e8eee0e4896
Anne Remacle <anne.m.remacle@gmail.com>
2016-12-16 12:30:19 +0100

    Extract components in separates files

--- static/modules/main.js
+++ static/modules/main.js
@@ -9,50 +9,15 @@
 // on importe vuejs
 import Vue from "vue";
 
-Vue.component( "cats-list", {
-    "props": [ "elements" ],
-    "template": `
-        <ul>
-            <li v-for="elt in elements">
-                <strong>{{ elt.name }}</strong>
-                <span>( {{ elt.age }} )</span>
-            </li>
-        </ul>
-    `,
-} );
-
-Vue.component( "secret", {
-    "props": [ "content" ],
-    "data": function() {
-        return {
-            "reveal": {
-                "show": "Reveal my secrets!",
-                "hide": "Hide my secrets",
-                "value": "Reveal my secrets!",
-            },
-            "state": false,
-        };
-    },
-    "template": `
-        <div>
-            <button v-on:click="revealSecret">{{ reveal.value }}</button>
-            <p v-if="state">{{ content }}</p>
-        </div>
-    `,
-    "methods": {
-        "revealSecret": function() {
-            this.state = !this.state;
-            this.reveal.value = this.state ? this.reveal.hide : this.reveal.show;
-        },
-    }
-} );
+import "./components/cats-list";
+import "./components/secret";
 
 let oApp = new Vue( {
     "template": `
         <div class="box">
             <p>{{ message }}</p>
             <cats-list v-bind:elements="cats"></cats-list>
-            <secret c-bind:content="secret"></secret>
+            <secret v-bind:content="secret"></secret>
         </div>`,
     "data": {
         "message": "Hey from Vue!",
