12630ab0c5db86cebd90d310696802582a383744
Anne Remacle <anne.m.remacle@gmail.com>
2016-12-16 12:21:10 +0100

    Extract components from template

--- static/modules/main.js
+++ static/modules/main.js
@@ -9,37 +9,58 @@
 // on importe vuejs
 import Vue from "vue";
 
+Vue.component( "cats-list", {
+    "props": [ "elements" ],
+    "template": `
+        <ul>
+            <li v-for="elt in elements">
+                <strong>{{ elt.name }}</strong>
+                <span>( {{ elt.age }} )</span>
+            </li>
+        </ul>
+    `,
+} );
+
+Vue.component( "secret", {
+    "props": [ "content" ],
+    "data": function() {
+        return {
+            "reveal": {
+                "show": "Reveal my secrets!",
+                "hide": "Hide my secrets",
+                "value": "Reveal my secrets!",
+            },
+            "state": false,
+        };
+    },
+    "template": `
+        <div>
+            <button v-on:click="revealSecret">{{ reveal.value }}</button>
+            <p v-if="state">{{ content }}</p>
+        </div>
+    `,
+    "methods": {
+        "revealSecret": function() {
+            this.state = !this.state;
+            this.reveal.value = this.state ? this.reveal.hide : this.reveal.show;
+        },
+    }
+} );
+
 let oApp = new Vue( {
     "template": `
         <div class="box">
             <p>{{ message }}</p>
-            <ul>
-                <li v-for="cat in cats">
-                    <strong>{{ cat.name }}</strong>
-                    <span>( {{ cat.age }} )</span>
-                </li>
-            </ul>
-            <button v-on:click="revealSecret">{{ reveal.value }}</button>
-            <p v-if="secret">I'm a cat person</p>
+            <cats-list v-bind:elements="cats"></cats-list>
+            <secret c-bind:content="secret"></secret>
         </div>`,
     "data": {
         "message": "Hey from Vue!",
-        "secret": false,
+        "secret": "I'm not a dog person!",
         "cats": [
             { "name": "Argus", "age": 4 },
             { "name": "Isis", "age": 8 },
         ],
-        "reveal": {
-            "show": "Reveal my secrets!",
-            "hide": "Hide my secrets",
-            "value": "Reveal my secrets!",
-        },
-    },
-    "methods": {
-        "revealSecret": function() {
-            this.secret = !this.secret;
-            this.reveal.value = this.secret ? this.reveal.hide : this.reveal.show;
-        },
     },
 } );
 
